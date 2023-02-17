4a6266b6cb1a21f59dc034f90e7f50ee66bf009a
Catarina <catisousalima1999@gmail.com>
2023-02-17 12:39:57 +0000

    all done now

--- src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
+++ src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
@@ -6,12 +6,67 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class IntStackTest {
+    private IntStack stack;
+    private int capacity;
+    @Before
+    public void constructStackObject(){
+        stack = new IntStack();
+        capacity = stack.getCapacity();
+    }
 
     @Test
     public void testNewStackIsNotFull() {
-        IntStack stack= new IntStack();
-
         assertFalse(stack.isFull());
+    }
+    @Test
+    public void testFullStackIsFull(){
+        int j = 0;
+        while (j<capacity){
+            stack.push(1);
+            j++;
+        }
+        assertTrue(stack.isFull());
+    }
+
+    public void testCornerCase(){
+        int cornerEnd = capacity - 1;
+        int j = 0;
+        while (j < cornerEnd){
+            stack.push(2);
+            j++;
+        }
+        assertFalse(stack.isFull());
+    }
+    @Test
+    public void testPopReturnsPushedValue(){
+        stack.push(1);
+        stack.pop();
+        assertEquals(1, stack.pop());
+    }
+    @Test (expected= ArrayIndexOutOfBoundsException.class)
+    public void throwsExceptionCornerCasePop(){
+        stack.pop();
+    }
+    @Test (expected= ArrayIndexOutOfBoundsException.class)
+    public void cornerCasePush(){
+        int j = 0;
+        while (j<capacity){
+            stack.push(2);
+            j++;
+        }
+        stack.push(2);
+    }
+    @Test
+    public void isEmptyTestWhenEmpty(){
+        assertTrue(stack.isEmpty());
+    }
+    @Test
+    public void isEmptyTestWhenFull(){
+        int j = 0;
+        while (j< capacity){
+            stack.push(1);
+        }
+        assertFalse(stack.isEmpty());
     }
 
 }
